0639d6b18b941aa0bddadf513e7e90fb15dcc8fa
Tyler Oscarson <tyler@theshortb.us>
2019-02-06 17:50:16 -0700

    Updated something..

--- drawtwo.php
+++ drawtwo.php
@@ -43,7 +43,7 @@ function calc_card_total($card_array1){
     <?php foreach($card_array as $card) : ?>
        <img src="<?php echo $card['image'];?>">
     <?php endforeach; ?>
-    <h1><?php echo "Your card total is $card_total"; ?></h1>
+    <h1><?php echo "Your card total is $card_total."; ?></h1>
     <?php if($card_total > 21): ?>
        Sorry your total is above 21
        <a href="index.php">Play Again</a>
